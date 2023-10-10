7cf7b030a607aa7da0ed15e54f67f835c07f4622
bikramdhiman <98263137+bikramdhiman@users.noreply.github.com>
2023-10-10 10:49:07 +0530

    Add files via upload

---
+++ Authentication/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const EmployeeModel = require("./models/Employee");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+mongoose.connect("mongodb://localhost:27017/employee");
+
+app.post("/register", (req, res) => {
+  EmployeeModel.create(req.body)
+    .then((employees) => res.json(employees))
+    .catch((err) => res.json(err));
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  EmployeeModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("the password is incorrect");
+      }
+    } else {
+      res.json("No id exist");
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("server is running");
+});
